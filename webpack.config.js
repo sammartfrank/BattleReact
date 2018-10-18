ecf8fd5471897dc7a3779ed1f49550774ed772cb
sammartfrank <franksammartino7@gmail.com>
2018-10-18 18:27:04 -0300

    'doingit

--- webpack.config.js
+++ webpack.config.js
@@ -14,13 +14,13 @@ module.exports = {
 			{ test: /\.(css)$/, use: [ 'style-loader', 'css-loader']}
 		]
 	},
-	devServer: {
-		historyApiFallback: true
-	},
 	mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: 'app/index.html'
 		})
-	]
+	],
+	devServer: {
+		historyApiFallback: true
+	},
 };
